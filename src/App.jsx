162ff6e8db4f33612f28d217ddb30bcc1bc43fd4
CPdA-Omi <charlie.perrondarc@gmail.com>
2024-04-22 09:39:12 +0200

    feat: Add api fetch Until any release, the number of elements fetched is limited

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+import { amiiboList } from './api/amiiboList.jsx';
+
 function App() {
   const [count, setCount] = useState(0)
 
