8b776a67b810957a0d22f5a2982a75dd0cf417eb
5p3c7r4l <enderspectral69150@gmail.com>
2024-04-28 20:21:28 +0200

    feat: Stage + data selection
    Started working on the visual of the data, will implement buttons with images at first then later will fetch the data from the Data branch to get the actual data

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,20 @@
 import { useState } from 'react'
 import './App.css'
 import Stage from './Stage'
+import VisualStage from './VisualStage'
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <Stage
-      x ={3}
-      y ={3}
-      />
-    </>
-  )
+    return (
+        <>
+        <VisualStage/>
+        <Stage
+        x ={8}
+        y ={8}
+        />
+        </>
+    )
 }
 
 export default App
--- src/Stage.jsx
+++ src/Stage.jsx
@@ -2,21 +2,57 @@ import React from "react";
 import { useEffect } from "react";
 import { useCallback } from "react";
 import { useState } from "react";
+import template from './template.jsx'
 
 export default function Stage ({x,y}){
-    console.log(x,y);
+
+    /*First initialisation of the stage with placeholder cards inside them*/
+    const stage =  []
+    stage.push({offset: x})
+    stage.push({
+        stageTotalSlot: Array
+        .apply(null, Array(x*y))
+        .map(()=>{return template()})
+    })
+
+
+    /*List comprehension function used to navigate through the stage*/
+    const listNav = (tile,list,evaluate,mode) => {
+
+        /*takes the current stage object*/
+            const stageObjects = Object
+            .values(stage.at(1))
+            .at(0)
+
+        /*chose an entire array from one of the stage's index*/
+            const stageObjIndex = stageObjects.at(tile)
+
+        const stageChoseIndex = Object
+            .entries(Object
+                .entries(stageObjIndex)
+                .at(0)
+                .at(1))
+
+        /*chose one of the list within the selected array (typically [0,1,2])*/
+            const stageChoseList = stageChoseIndex.at(list)
+
+        /*chose one of the value within the selected list*/
+            const stageChoseListKeyVal = Object
+            .entries(stageChoseList
+                .at(1))
+            .at(evaluate)
+            .at(1)
+
+        switch(mode){
+        case 0:
+            return stageChoseIndex
+        case 1:
+            return stageChoseList
+        case 2:
+            return stageChoseListKeyVal
+        }
+    }
+    console.clear()
+    console.log(listNav(0,0,0,0))
     
-    const stage=[];
-    const tmp= []
-    for (let i=0; i < x; i++){
-        tmp.push({
-            "key":i
-    });
-    }
-    console.log(tmp);
-
-
-    for (let i=0; i<y; i++){
-        stage.push(tmp)
-    }
 }
